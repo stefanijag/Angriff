47f5bf84341e79576fb0108455ae52e506ddfc08
Stefanija G <stefig_2000@hotmail.com>
2019-11-09 12:14:55 +0100

    implemented all functionalities

--- src/main/java/myprojects/angriff/ui/LoginController.java
+++ src/main/java/myprojects/angriff/ui/LoginController.java
@@ -2,6 +2,8 @@ package myprojects.angriff.ui;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -19,6 +21,7 @@ import myprojects.angriff.service.hibbean.LoginHIBBean;
 public class LoginController {
 	
 	ApiPersistenceAPI api = new ApiPersistenceDBImpl();
+	private static Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 	
 	@RequestMapping(value="/login")
 	public String index(@Valid@ModelAttribute("login")LoginBean loginBean, BindingResult result){
@@ -35,22 +38,27 @@ public class LoginController {
 		String role = api.login(new LoginHIBBean(loginBean.getUsername(), loginBean.getPassword()));
 		if(role.isEmpty()) {
 			model.addAttribute("error", "Benutzername oder Passwort ist nicht korrekt!");
+			LOGGER.error("Username: " +loginBean.getUsername()+ " or Password: " +loginBean.getPassword() +" were not correct");
 			return new ModelAndView("/login/viewLogin");
 		}else if(role.equals("AD")) {
-			System.out.println("admin");
+			LOGGER.info("Successfully logged in as an admin");
 			return new ModelAndView("/project/overviewProject");
 		}
+		LOGGER.info("Successfully logged in as an user");
 		return new ModelAndView("/project/overviewProject");
 	}
 	
 	@RequestMapping(value="/register/submit")
-	public ModelAndView registration(@Valid@ModelAttribute("register")RegisterUIBean registerUIBean, BindingResult result) {
+	public ModelAndView registration(@Valid@ModelAttribute("register")RegisterUIBean registerUIBean, BindingResult result, ModelMap model) {
 		if(registerUIBean.getPassword().equals(registerUIBean.getConfirmPassword())){
 			LoginHIBBean loginBean = new LoginHIBBean(registerUIBean.getUsername(), registerUIBean.getPassword(), registerUIBean.getEmail(),
 					registerUIBean.getName(), registerUIBean.getLastname());
 			api.register(loginBean);
+			LOGGER.info("Successfully registered as an user");
 			return new ModelAndView("/login/viewRegister");
 		}
+		model.addAttribute("error", "Passwort und Passwort bestätigen stimmt nicht überein");
+		LOGGER.debug("Password and confirm password does not match");
 		return new ModelAndView("/login/viewRegister");
 	}
 	
